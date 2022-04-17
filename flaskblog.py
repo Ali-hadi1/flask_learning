59d99cc2d96162086ca21e38c43fca1607d1726f
Ali-hadi1 <hadiadelzada2011@gmail.com>
2022-04-17 14:44:51 +0430

    add files

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Ali',
+        'title': 'blog post 1',
+        'content': 'first post content',
+        'date_posted': 'april 20 2020'
+    },
+    {
+        'author':'hadi',
+        'title': 'blog post 2',
+        'content': 'second post content',
+        'date_posted': 'april 20 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts= posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__=='__main__':
+    app.run(debug=True)  
