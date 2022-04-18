9fdee89a29e212448e087acc7aaac5df25b80db3
Ali-hadi1 <hadiadelzada2011@gmail.com>
2022-04-18 12:12:01 +0430

    changes

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1caae790185db9529f000ce7b22d83ab'
 
 posts = [
     {
@@ -25,6 +27,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegisterationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for { form.username.data }!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form =form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', form =form)
+
 
 if __name__=='__main__':
     app.run(debug=True)  
